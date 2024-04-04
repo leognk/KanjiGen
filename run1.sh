f48babf646729fa387f04e6af1b047f684b007e7
leognk <leotaisei@gmail.com>
2024-04-04 17:06:40 +0000

    updated

--- run1.sh
+++ run1.sh
@@ -9,7 +9,7 @@ accelerate launch --mixed_precision="fp16" train_text_to_image.py \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
-  --max_train_steps=5 \
+  --max_train_steps=15000 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
