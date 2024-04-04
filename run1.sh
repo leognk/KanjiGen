55f78c996dcd61ebca1a62a807bb848b7a1789e1
leognk <leotaisei@gmail.com>
2024-04-04 20:44:21 +0000

    updated

--- run1.sh
+++ run1.sh
@@ -6,12 +6,10 @@ accelerate launch --mixed_precision="fp16" train_text_to_image.py \
   --train_data_dir=$TRAIN_DIR \
   --use_ema \
   --resolution=128 --center_crop --random_flip \
-  --train_batch_size=1 \
-  --gradient_accumulation_steps=4 \
-  --gradient_checkpointing \
-  --max_train_steps=15000 \
+  --train_batch_size=32 \
+  --num_train_epochs=2 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="exp/sd-kanji-model" \
+  --output_dir="exp/model1" \
   --report_to="wandb"
