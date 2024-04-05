0116c1b6a8956df8ed232fb7478dd3a090ccd165
leognk <leotaisei@gmail.com>
2024-04-05 08:20:22 +0000

    updated

--- train_text_to_image.py
+++ train_text_to_image.py
@@ -1122,12 +1122,12 @@ if __name__ == "__main__":
     --train_data_dir=data/images
     --use_ema
     --resolution=128 --center_crop --random_flip
-    --train_batch_size=32
-    --num_train_epochs=2
+    --train_batch_size=16
+    --num_train_epochs=100
     --checkpointing_steps=10000
-    --learning_rate=1e-03
+    --learning_rate=1e-04
     --max_grad_norm=1
-    --lr_scheduler=constant --lr_warmup_steps=0
+    --lr_scheduler=cosine --lr_warmup_steps=500
     --output_dir=exp/model1
     --report_to=wandb
     """.split()
